3b9a23c61cecc4d7ec4ebbcf7b3a0b00ccc580ce
wonderboy002 <nipunkup@gmail.com>
2024-01-25 23:33:23 +0530

    BST BASICS DONE

--- BST/Creation.cpp
+++ BST/Creation.cpp
@@ -59,11 +59,105 @@ void inorder(Node *root)
     inorder(root->right);
 }
 
+int minimum(Node *root){
+    while (root->left){
+        root=root->left;
+    }
+    return root->data;
+}
+
+int maximum(Node *root){
+    while (root->right){
+        root=root->right;
+    }
+    return root->data;
+}
+
+bool SearchInBST(Node *root,int key){
+    //base cases
+    if (root==NULL){
+        return false;
+    }
+
+    if (root->data==key){
+        return true;
+    }
+    
+    // search in left or right subtrees
+    return SearchInBST(root->left,key) || SearchInBST(root->right,key);
+}
+
+Node *DeleteFromBST(Node *root,int x){
+    if (root==NULL){
+        return NULL;
+    }
+
+    if (root->data==x){
+       //4 cases for node deletion
+
+       //case 1 leaf node
+       if (!root->left && !root->right){
+        return NULL;
+       }
+
+       //case 2 left null and right not null
+       else if (!root->left && root->right){
+        Node *child=root->right;
+        delete root;
+        return child;
+       }
+
+       //case 3 left not null and right null
+       else if (!root->right && root->left){
+        Node *child=root->left;
+        delete root;
+        return child;
+       }
+       
+       //case 4 a proper node 
+       else {
+          int LST=maximum(root->left);
+          root->data=LST;
+          root->left=DeleteFromBST(root->left,LST);
+       }
+    }
+
+    else if (root->data>x){
+        root->left=DeleteFromBST(root->left,x);
+    }
+    else {
+        root->right=DeleteFromBST(root->right,x);
+    }
+
+    return root;
+}
+
 int main()
 {
     Node *root = NULL;
     createTree(root);
     cout << "BST successfully created" << endl;
+    inorder(root);
+    cout<<endl;
+
+    //  50 30 60 20 40 25 55 70 80 -1
+
+    cout<<"Minimum of BST : "<<minimum(root)<<endl;
+    cout<<"Maximum of BST : "<<maximum(root)<<endl;
+
+    int key;
+    cout<<"Enter key to search in BST : "<<endl;
+    cin>>key;
+
+    bool ans=SearchInBST(root,key);
+    ans==true?cout<<"Value exists in BST":cout<<"Value does not exist in BST"<<endl;
+
+    int x;
+    cout<<"enter a key to delete from BST"<<endl;
+    cin>>x;
+
+    root=DeleteFromBST(root,x);
+    cout<<"BST after deletion : "<<endl;
     inorder(root);
     return 0;
 }
